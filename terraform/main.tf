52e8d9ef4522b8a791a33bd81c1822167f014473
Kinan-Benfrid <77750495+Kinan-Benfrid@users.noreply.github.com>
2022-12-06 16:22:49 +0100

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "ec2_vm" {
+  ami           = "ami-02384a901b5df8024"
+  instance_type = "t3.micro"
+  key_name      = "mykey"
+  tags = {
+    Name = "ExempleInstanceEC2Linux"
+    Owner = "PhCurmin"
+    Etudiant= "kinan.benfrid@etu.u-pec.fr"
+  }
+  
+  vpc_security_group_ids = ["${aws_security_group.default.id}"] 
+}
+
+# Default ec2 user is "ec2-user"
